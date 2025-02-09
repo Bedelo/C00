ca41007afdfa2edbd81f86c2053644412c10be19
Bedelo Sunda <bsunda@bess-f4r5s2.clusters.42paris.fr>
2025-02-09 16:50:42 +0100

    c++ manner

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -1,26 +1,24 @@
 #include <iostream> 
 #include <string>  
-#include <stdio.h>
-
-void toUpperCase(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		std::cout << static_cast<char> (toupper(str[i++]));
-}
 
 int	main(int ac, char **av)
 {
 	int		i;
+	unsigned long		c;
+	std::string str;
 
 	i = 1;
+	c = 0;
 	if (ac == 1)
 		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
 	while (av[i])
 	{
-		toUpperCase(av[i]);
+		c = 0;
+		str = av[i];
+		while (c < str.length() ){
+			std::cout << static_cast<char> (std::toupper(str[c]));
+			c++;
+		}
 		i++;
 	}
 	std::cout << std::endl;
