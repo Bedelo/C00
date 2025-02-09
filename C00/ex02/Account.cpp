9736c4963b45ffd647c0bc11ec276f3a2ca82e94
Bedelo Sunda <bsunda@bess-f4r5s2.clusters.42paris.fr>
2025-02-09 10:55:35 +0100

    refacto ex02 display fixed

--- C00/ex02/Account.cpp
+++ C00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: bsunda <bsunda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 13:18:23 by bsunda            #+#    #+#             */
-/*   Updated: 2025/01/20 11:22:06 by bsunda           ###   ########.fr       */
+/*   Updated: 2025/02/09 10:50:57 by bsunda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ Account::Account( int initial_deposit ){
 
 Account::~Account( void ){
 	_displayTimestamp();
-	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount << "; closed" << std::endl;
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
 	return ;
 }
 
@@ -93,7 +93,7 @@ bool	Account::makeWithdrawal( int withdrawal ){
 	Account::_displayTimestamp();
 	std::cout << " index:" << this->_accountIndex << ";p_amount:" << this->_amount;
 	if (withdrawal > this->_amount){
-		std::cout << ";withdrawal: refused" << std::endl;
+		std::cout << ";withdrawal:refused" << std::endl;
 		return false;
 	}
 	else
@@ -113,7 +113,7 @@ int		Account::checkAmount( void ) const{
 
 void	Account::displayStatus( void ) const{
 	Account::_displayTimestamp();
-	std::cout << " index" << this->_accountIndex << ";amount:" << this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
 void	Account::displayAccountsInfos( void ){
